6863a71d131dfb862c8525c5c6d0d0432ec861ca
Heungseok Park <heungseok2@gmail.com>
2017-08-24 13:57:25 +0900

    assign node attribute using course data

--- js/main.js
+++ js/main.js
@@ -19,10 +19,14 @@ var line = d3.line()
     // .x(function(d){ console.log(d); return x(d)})
     // .y(function(d){ return y(d)});
 
+var course_info;
 var nodes,
     links;
 
-// tooltip
+var current_time = 3;
+var current_platform = "classCentral";
+
+// ********** tooltip *************
 var tooltip = d3.select("#d3_container").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
@@ -40,111 +44,166 @@ function initD3(){
 
     // d3.json("./data/modularity_k4_CPM_perf_log_t3_cc.json", function(error, graph){
     d3.json("./data/C19_color_encoded(good3).json", function(error, graph){
-        console.log(graph);
 
-        nodes = graph.nodes.map(function(d){
-            return {
-                'index' : d.id,
-                'x' : d.x,
-                'y' : d.y,
-                'area' : d.attributes.area,
-                'subject': d.attributes.subject,
-                'color': d.color
-            }
+        d3.csv("./data/MOOC_parcoord_data_only_net_available.csv", function(courses){
+            console.log(courses);
+
+
+            // ************* 1. Course data parsing by global time variable ************ //
+
+
+            course_info = _.where(courses, {time: String(current_time), review_platform: current_platform });
+
+
+            console.log(graph);
+
+            nodes = graph.nodes.map(function(d){
+
+                var res_findWhere = _.findWhere(course_info, {url: d.attributes.name });
+
+                if(res_findWhere !== undefined){
+                    return {
+
+                        // node info
+                        'index' : d.id,
+                        'x' : d.x,
+                        'y' : d.y,
+                        'color': d.color,
+
+                        // 나중에 community data 도 필요함: community id, # of overlapping community,
+                        // 그리고 rank, centrality 등 모두 통일된 range로 스케일 조정 필수.
+                        'community': d.attributes.community,
+                        'overlap_num': d.attributes.overlap_num,
+
+                        // course info
+                        'area': res_findWhere.area,
+                        'subject': res_findWhere.subject,
+                        'school': res_findWhere.school
+
+
+
+                    }
+                }else{
+                    return {
+                        // node info
+                        'index' : d.id,
+                        'x' : d.x,
+                        'y' : d.y,
+                        'color': d.color,
+
+                        'community': d.attributes.community,
+                        'overlap_num': d.attributes.overlap_num,
+
+                        // course info
+                        'area' : d.attributes.area,
+                        'subject': d.attributes.subject,
+                        'school': "unknown"
+                    }
+                }
+
+            });
+
+            console.log(nodes);
+
+            // set the x, y domain using _.pluck
+            // var x0 = _.pluck(graph.nodes, 'x');
+            // var y0 = _.pluck(graph.nodes, 'y');
+            var x0 = d3.extent(nodes, function(d){ return d.x;});
+            var y0 = d3.extent(nodes, function(d){ return d.y;})
+
+            // 상화좌우로 100씩 확장. 실패.
+            // x.domain([ x0[0]-1000, x0[1]+1000 ]);
+            // y.domain([ y0[0]-1000, y0[1]+1000 ]);
+            x.domain(x0);
+            y.domain(y0);
+            // y.domain(d3.extent(nodes, function(d){ return d.y;}));
+
+
+            links = graph.edges.map(function(d){
+                return {
+                    'source': d.source,
+                    'target': d.target,
+                    'color': d.color
+
+                }
+            });
+
+            /*
+             // draw each edge using line
+             g.append("g")
+             .attr("class", "edges")
+             .attr("stroke", "#000")
+             .attr("stroke-width", 0.5)
+             .selectAll("line")
+             .data(links)
+             .enter().append("line")
+             .attr("stroke", function(d) { return d.color; })
+             .attr("x1", function(d) { return x(findNodePositionX(d.source)); })
+             .attr("y1", function(d) { return y(findNodePositionY(d.source)); })
+             .attr("x2", function(d) { return x(findNodePositionX(d.target)); })
+             .attr("y2", function(d) { return y(findNodePositionY(d.target)); });
+             */
+
+            // draw each edge using path
+            g.append("g")
+                .attr("class", "edges")
+                .attr("stroke", "#000")
+                .attr("stroke-width", 0.5)
+                .selectAll("line")
+                .data(links)
+                .enter().append("path")
+                .attr("class", "line")
+                .attr("stroke-width", function(d) { return Math.sqrt(d.size); })
+                .attr("stroke", function(d) { return d.color; })
+                .attr("d", function(d) { return line( [
+                    [x(findNodePositionX(d.source)), y(findNodePositionY(d.source))],
+                    [x(findNodePositionX(d.target)), y(findNodePositionY(d.target))]
+                ])});
+
+            g.append("g")
+                .attr("class", "nodes")
+                .attr("stroke", "#fff")
+                .attr("stroke-width", 0.01)
+                .selectAll("circle")
+                .data(nodes)
+                .enter().append("circle")
+                .attr("fill", function(d) { return d.color; })
+                .attr("cx", function(d) { return x(d.x); })
+                .attr("cy", function(d) { return y(d.y); })
+                .attr("r", function(d) { return 0.001 + (+d.overlap_num); })
+                .on("mouseover", function(d) {
+                    tooltip.transition()
+                        .duration(100)
+                        .style("opacity", .9)
+                        .style("display", "block");
+
+                    tooltip.html(
+                        "Community: " + d.community + "</br>" +
+                        "# Overlap: " + parseInt(d.overlap_num) + "</br>" +
+                        "Area: " + d.area + "</br>" +
+                        "Subject: " + d.subject + "</br>" +
+                        "School: " + d.school + "</br>"
+
+                    )
+                        .style("left", (d3.event.pageX + 5) + "px")
+                        .style("top", (d3.event.pageY - 28) + "px");
+                })
+                .on("mouseout", function(d) {
+                    tooltip.transition()
+                        .duration(100)
+                        .style("opacity", 0)
+                        .style("display", "none");
+
+                })
+                .call(d3.drag()
+                    .on("drag", dragged));
+
+            svg.call(d3.zoom()
+                .scaleExtent([1 / 2, 8])
+                .on("zoom", zoomed))
+
+
         });
-
-        // set the x, y domain using _.pluck
-        // var x0 = _.pluck(graph.nodes, 'x');
-        // var y0 = _.pluck(graph.nodes, 'y');
-        var x0 = d3.extent(nodes, function(d){ return d.x;});
-        var y0 = d3.extent(nodes, function(d){ return d.y;})
-
-        // 상화좌우로 100씩 확장. 실패.
-        // x.domain([ x0[0]-1000, x0[1]+1000 ]);
-        // y.domain([ y0[0]-1000, y0[1]+1000 ]);
-        x.domain(x0);
-        y.domain(y0);
-        // y.domain(d3.extent(nodes, function(d){ return d.y;}));
-
-
-        links = graph.edges.map(function(d){
-            return {
-                'source': d.source,
-                'target': d.target,
-                'color': d.color
-
-            }
-        });
-
-/*
-        // draw each edge using line
-        g.append("g")
-            .attr("class", "edges")
-            .attr("stroke", "#000")
-            .attr("stroke-width", 0.5)
-            .selectAll("line")
-            .data(links)
-            .enter().append("line")
-            .attr("stroke", function(d) { return d.color; })
-            .attr("x1", function(d) { return x(findNodePositionX(d.source)); })
-            .attr("y1", function(d) { return y(findNodePositionY(d.source)); })
-            .attr("x2", function(d) { return x(findNodePositionX(d.target)); })
-            .attr("y2", function(d) { return y(findNodePositionY(d.target)); });
-*/
-
-        // draw each edge using path
-        g.append("g")
-            .attr("class", "edges")
-            .attr("stroke", "#000")
-            .attr("stroke-width", 0.5)
-            .selectAll("line")
-            .data(links)
-            .enter().append("path")
-            .attr("class", "line")
-            .attr("stroke", function(d) { return d.color; })
-            .attr("d", function(d) { return line( [
-                [x(findNodePositionX(d.source)), y(findNodePositionY(d.source))],
-                [x(findNodePositionX(d.target)), y(findNodePositionY(d.target))]
-            ])});
-
-        g.append("g")
-            .attr("class", "nodes")
-            .attr("stroke", "#fff")
-            .attr("stroke-width", 0.01)
-            .selectAll("circle")
-            .data(nodes)
-            .enter().append("circle")
-            .attr("fill", function(d) { return d.color; })
-            .attr("cx", function(d) { return x(d.x); })
-            .attr("cy", function(d) { return y(d.y); })
-            .attr("r", 2.5)
-            .on("mouseover", function(d) {
-                tooltip.transition()
-                    .duration(100)
-                    .style("opacity", .9)
-                    .style("display", "block");
-
-                tooltip.html(
-                    "Area: " + d.area +
-                    "</br> Subject: " + d.subject +
-                    "</br>"
-                )
-                    .style("left", (d3.event.pageX + 5) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-            .on("mouseout", function(d) {
-                tooltip.transition()
-                    .duration(100)
-                    .style("opacity", 0)
-                    .style("display", "none");
-
-            })
-            .call(d3.drag()
-                .on("drag", dragged));
-
-        svg.call(d3.zoom()
-            .scaleExtent([1 / 2, 8])
-            .on("zoom", zoomed))
 
     });
 
